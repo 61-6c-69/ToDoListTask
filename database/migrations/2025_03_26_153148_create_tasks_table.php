420a3fda0bd8c7ca62e7bdef32e487a515e42269
ali <alifarshad13@gmail.com>
2025-03-26 19:13:57 +0330

    set task columns

--- database/migrations/2025_03_26_153148_create_tasks_table.php
+++ database/migrations/2025_03_26_153148_create_tasks_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+			$table->string('title');
+			$table->text('description')->nullable();
+			$table->enum('status', ['pending', 'progress', 'completed'])->default('pending');
+			$table->date('due_date')->nullable();
+			$table->enum('priority', ['low', 'medium', 'high'])->default('medium');
+			
             $table->timestamps();
         });
     }
