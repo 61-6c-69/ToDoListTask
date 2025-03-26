5d7ba8ca9faf0bd6eb0440c6a3f62f9efdeed410
ali <alifarshad13@gmail.com>
2025-03-27 00:17:27 +0330

    auth methods

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,4 +7,11 @@ use Illuminate\Http\Request;
 class AuthController extends Controller
 {
     //
+	public function Register(Request $request){
+		return [];
+	}
+	
+	public function Login(Request $request){
+		return [];
+	}
 }
