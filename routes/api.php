df11d86d65130ab82a0b80f13612ce9d7809ec28
ali <alifarshad13@gmail.com>
2025-03-27 00:17:47 +0330

    auth routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
+
+/*
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+*/
+
+Route::prefix('v1')->group(function(){
+	Route::prefix('auth')->group(function(){
+		Route::post('register', [AuthController::class, 'Register']);
+		Route::post('login', [AuthController::class, 'Login']);
+	});
+});
