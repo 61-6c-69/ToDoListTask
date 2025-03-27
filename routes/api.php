2d3a5fdcf5eec26497ffa97e9d0cd5585e77d410
ali <alifarshad13@gmail.com>
2025-03-27 18:23:23 +0330

    create task api route

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 
 
 /*
@@ -16,5 +17,12 @@ Route::prefix('v1')->group(function(){
 	Route::prefix('auth')->group(function(){
 		Route::post('register', [AuthController::class, 'Register']);
 		Route::post('login', [AuthController::class, 'Login']);
+	});
+	
+	Route::prefix('user')->middleware('auth:sanctum')->group(function(){
+		Route::get('/{id?}', [TaskController::class, 'Show']);
+		Route::post('create', [TaskController::class, 'Store']);
+		Route::put('update', [TaskController::class, 'Update']);
+		Route::delete('delete', [TaskController::class, 'Delete']);
 	});
 });
