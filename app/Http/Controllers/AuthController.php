3311b82cdfd59b5d166affb8585322123dd05cba
ali <alifarshad13@gmail.com>
2025-03-27 11:52:48 +0330

    add [register] method

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,15 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 
 class AuthController extends Controller
 {
     //
-	public function Register(Request $request){
-		return [];
+	public function Register(RegisterRequest $request): JsonResponse{
+		$user = User::create($request->only('name', 'email', 'password'));
+		
+		return response()->json([
+			'status' => 'success',
+			'user' => $user
+		], Response::HTTP_CREATED);
 	}
 	
-	public function Login(Request $request){
+	public function Login(Request $request): JsonResponse{
 		return [];
 	}
 }
