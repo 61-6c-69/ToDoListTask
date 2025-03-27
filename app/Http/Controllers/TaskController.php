816e22713f384ac35debfd0e8eff11d454bceef6
ali <alifarshad13@gmail.com>
2025-03-27 18:42:43 +0330

    create [create] task method

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -6,12 +6,17 @@ use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Illuminate\Http\JsonResponse;
 use App\Models\Tasks;
+use App\Http\Requests\TaskRequest;
 
 class TaskController extends Controller
 {
     //
-	public function Store(){
-		return;
+	public function Store(TaskRequest $request): JsonResponse{
+		$task = auth()->user()->tasks()->create($request->all());
+		
+		return response()->json([
+			'task' => $task
+		], Response::HTTP_CREATED);
 	}
 	
 	public function Show(){
