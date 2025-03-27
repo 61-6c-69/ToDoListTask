0466e7eacce0d093e048bea4a8440e3a0b03860b
ali <alifarshad13@gmail.com>
2025-03-27 18:17:51 +0330

    create methods

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\JsonResponse;
+use App\Models\Tasks;
 
 class TaskController extends Controller
 {
     //
+	public function Store(){
+		return;
+	}
+	
+	public function Show(){
+		return;
+	}
+	
+	public function Update(){
+		return;
+	}
+	
+	public function Delete(){
+		return;
+	}
 }
