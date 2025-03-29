d47dfb461613ec62691c2f6f96a0e6992691f2cd
ali <alifarshad13@gmail.com>
2025-03-29 17:16:30 +0330

    update sizes

--- resources/js/layouts/DashboardLayout.jsx
+++ resources/js/layouts/DashboardLayout.jsx
@@ -7,17 +7,17 @@ import ShowTasks from './../components/ShowTasks'
 export default function AuthLayout(props){
 	
 	return(
-		<div className='card bg-base-100 shadow-lg w-[1000px] m-auto'>
+		<div className='card bg-base-100 shadow-lg w-full md:w-[1000px] m-auto'>
 			<div className='card-body'>
 				<div className='card-title'>
 					<Navbar/>
 				</div>
 				
-				<div className='grid grid-cols-3 gap-4'>
+				<div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
 					<div>
 						<CreateTask/>
 					</div>
-					<div className='col-span-2'>
+					<div className='col-span-2 h-[400px] md:h-full'>
 						<ShowTasks/>
 					</div>
 				</div>
